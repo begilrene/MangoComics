e785cfe00a3149752bd606237ddae051cfa2e016
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-01 23:44:51 +0000

    show per issue not static 10 issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    client = Issue.first.all
-    render html: client
+    render html: "Hello, world!"
+  end
+  def comics
+    @comiclist = Issue.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-   resources:schema #mangoDB
-  resources:comics
+  resources:schema #mangoDB
+  resources:issues
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#help'
 end
