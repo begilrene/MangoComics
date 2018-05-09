b29f5879d8e8cd5f595dd7da99f2d2937684dd3d
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-09 06:43:44 +0000

    added some issue stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to static_pages_help_url
       # Handle a successful save.
     else
       render 'new'
