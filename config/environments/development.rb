ef336b31cde586cfaa69bee9f29b3f9426631416
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-10 07:57:43 +0000

    gemfile change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.web_console.whitelisted_ips = '47.145.192.42'
   # Do not eager load code on boot.
   config.eager_load = true
 
