9c405d1d5922f8a92be583be0f4dd5e55abf0e1c
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-10 06:52:28 +0000

    sorta working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    @users = User.all
+    @comics = Issue.all
    # p = User.new
    # String hmm = ('a'..'z').to_a.shuffle[0..7].join
    # p.username = hmm
    # render html: p.username
- end
+  end
  
  
   def help
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,14 +3,13 @@ module SessionsHelper
     session[:userID] = user.userID
   end
   def current_user
-   @current_user ||= User.find_by(userID: session[:userID])
+    @current_user ||= User.find_by(userID: session[:userID])
   end
   def logged_in?
     !current_user.nil?
   end
   def log_out
-    @current_user = nil
     session.delete(:userID)
-    #@current_user = nil
+    @current_user = nil
   end
 end
