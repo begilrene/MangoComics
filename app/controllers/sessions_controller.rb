42d6663e6dec4291c5c1b0328b9f96c3ec6b8b79
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-10 07:04:51 +0000

    it works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to static_pages_help_url
+      redirect_to root_url
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
