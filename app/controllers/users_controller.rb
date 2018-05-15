4b9559a261cb45dbd05ba1c8e002711d55ac0e92
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-15 05:01:54 +0000

    more user fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
+  end
+  def index
+    @users = User.all
   end
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,18 +21,35 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
   def edit
     @user = User.find(params[:id])
   end
+  
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to static_pages_home_url
+      flash[:success] = "Profile updated"
+      redirect_to @user
       # Handle a successful update.
     else
       render 'edit'
     end
   end
+  
+  def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
+  
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+  end
+  
   def destroy
   end
   
@@ -33,6 +57,6 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:displayName, :email, :password,
-                                   :password_confirmation, :personalBio)
+                                   :password_confirmation, :personalBio, :firstname, :lastname, :profilePic)
     end
 end
