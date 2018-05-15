174cfd7d618bae4bb141ea90e388009be7373da8
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-15 05:01:54 +0000

    more user fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_back_or root_url
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,8 +8,21 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+  def current_user?(user)
+    user == current_user
+  end
   def log_out
     session.delete(:userID)
     @current_user = nil
+  end
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
