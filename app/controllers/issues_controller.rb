3b1940a02fb8f27ee2ee204802cbfe671b231149
skh28 <scottha28@gmail.com>
2018-05-22 02:46:53 +0000

    fxing conflicts

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,5 @@
 class IssuesController < ApplicationController
+    @iss = Issue.all
   def new
   end
   def show
@@ -9,8 +10,13 @@ class IssuesController < ApplicationController
   def comment_params
       params.permit(:body,:user_id)
   end
-  def index
-    redirect_to '/static_pages/comiclist'
-  end
   
+  def index
+  @iss = Issue.all
+  if params[:search]
+    @iss = Issue.search(params[:search]).order("created_at DESC")
+  else
+    @iss = Issue.all.order("created_at DESC")
+  end
+  end
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,16 @@
 class Issue < ActiveRecord::Base
     self.table_name = 'Issue'
+<<<<<<< HEAD
     self.primary_key = :issueID
     has_many :IComments
+=======
+    self.primary_key = :issueName
+
+    def self.search(search)
+    if search
+      losearch = search.downcase
+      where("lower(issueName) LIKE ?", "%#{search.to_s.downcase}%")
+    end
+    end
+>>>>>>> search
 end
