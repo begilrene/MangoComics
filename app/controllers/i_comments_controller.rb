2dea038ee3115c681383bf71127e947997f6e5b0
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-23 02:06:20 +0000

    added edit and delete to news

--- app/controllers/i_comments_controller.rb
+++ app/controllers/i_comments_controller.rb
@@ -1,4 +1,6 @@
 class ICommentsController < ApplicationController
+  before_action :get_issue_and_comment, only: [:destroy, :edit, :update, :is_owner]
+  before_action :is_owner, only: [:destroy, :edit, :update]
   def create
     @iss = Issue.find(params[:issue_id])
     @comm = Issue.find(@iss.issueID).IComments.new(comment_params)
@@ -9,23 +11,27 @@ class ICommentsController < ApplicationController
     end
   end
   def edit
-    @iss = Issue.find(params[:issue_id])
-    @comm = Issue.find(@iss.issueID).IComments.find(params[:id])
   end
   def update
-    @iss = Issue.find(params[:issue_id])
-    @comm = Issue.find(@iss.issueID).IComments.find(params[:id])
     if @comm.update_attributes(comment_params)
       redirect_to @iss
     end
   end
   def destroy
-    @iss = Issue.find(params[:issue_id])
-    @comm = Issue.find(@iss.issueID).IComments.find(params[:id]).destroy
+    @comm.destroy
     redirect_to @iss
   end
   private
   def comment_params
     params.require(:i_comment).permit(:body, :user_id)
+  end
+  def get_issue_and_comment
+    @iss = Issue.find(params[:issue_id])
+    @comm = Issue.find(@iss.issueID).IComments.find(params[:id])
+  end
+  def is_owner
+    unless current_user.id == @comm.user_id
+    redirect_to @iss
+    end
   end
 end
--- app/controllers/m_comments_controller.rb
+++ app/controllers/m_comments_controller.rb
@@ -1,4 +1,6 @@
 class MCommentsController < ApplicationController
+  before_action :get_news_and_comment, only: [:destroy, :edit, :update, :is_owner]
+  before_action :is_owner, only: [:destroy, :edit, :update]
   def create
     @news = New.find(params[:news_id])
     @comm = New.find(@news.newsId).MComments.new(comment_params)
@@ -8,7 +10,28 @@ class MCommentsController < ApplicationController
     else
     end
   end
+  def edit
+  end
+  def update
+    if @comm.update_attributes(comment_params)
+      redirect_to @news
+    end
+  end
+  def destroy
+    @comm.destroy
+    redirect_to @iss
+  end
+  private
   def comment_params
     params.require(:m_comment).permit(:body, :user_id)
+  end
+  def get_news_and_comment
+    @news = New.find(params[:news_id])
+    @comm = New.find(@news.newsId).MComments.find(params[:id])
+  end
+  def is_owner
+    unless current_user.id == @comm.user_id
+    redirect_to @new
+    end
   end
 end
