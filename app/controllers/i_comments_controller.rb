223b49cb8d10da28733b2f433697ee8dedfc4450
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-23 01:43:53 +0000

    added edit and delete to issues

--- app/controllers/i_comments_controller.rb
+++ app/controllers/i_comments_controller.rb
@@ -8,6 +8,23 @@ class ICommentsController < ApplicationController
     else
     end
   end
+  def edit
+    @iss = Issue.find(params[:issue_id])
+    @comm = Issue.find(@iss.issueID).IComments.find(params[:id])
+  end
+  def update
+    @iss = Issue.find(params[:issue_id])
+    @comm = Issue.find(@iss.issueID).IComments.find(params[:id])
+    if @comm.update_attributes(comment_params)
+      redirect_to @iss
+    end
+  end
+  def destroy
+    @iss = Issue.find(params[:issue_id])
+    @comm = Issue.find(@iss.issueID).IComments.find(params[:id]).destroy
+    redirect_to @iss
+  end
+  private
   def comment_params
     params.require(:i_comment).permit(:body, :user_id)
   end
