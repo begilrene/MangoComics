4d03b9e960ebc568c45882f5934a910103805f3f
MatthewTLi <matthewlitianyao@hotmail.com>
2018-05-25 01:34:47 +0000

    very nice search

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,7 +3,8 @@ class Issue < ActiveRecord::Base
     self.primary_key = :issueID
 
     def self.search(search)
-      where("'issueID' Like ?" , "%#{search}%") 
+      search = search.downcase
+      where("lower(issuename) Like ? OR lower(summary) Like ? OR lower(synopsis) Like ? OR lower(artist) Like ? OR lower(author) Like ? OR lower(editor) Like ?" , "%#{search}%" ,"%#{search}%","%#{search}%", "%#{search}%" ,"%#{search}%","%#{search}%")
     end
     has_many :IComments
 end
