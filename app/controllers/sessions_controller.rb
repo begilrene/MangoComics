433c8f8ff9f395c1e408c0c6a7ad792c6ded899f
MatthewTLi <matthewlitianyao@hotmail.com>
2018-06-01 03:35:42 +0000

    init

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      flash[:notice] = "Welcome"
       redirect_back_or root_url
       # Log the user in and redirect to the user's show page.
     else
