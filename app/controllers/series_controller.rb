ad42c2f0b3023b397aa7997098d66a01d629f262
MatthewTLi <matthewlitianyao@hotmail.com>
2018-06-03 23:36:36 +0000

    series,issue,volume new and update should be wokrign

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,10 +1,27 @@
 class SeriesController < ApplicationController
   def new
+    @ser = Series.new
+  end
+  def create
+    @ser = Series.new(series_params)
+    if @ser.save
+      redirect_to @ser
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @ser = Series.find(params[:id])
   end
   def show
     @ser = Series.find(params[:id])
     @rating = @ser.SRatings.star(params[:id])
     @comm = @ser.SComments
     store_location
+  end
+  private
+  def series_params
+     params.require(:series).permit(:seriesname,:picture,:franchise_id)
   end
 end
