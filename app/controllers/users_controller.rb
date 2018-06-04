5a44cb4595608461248b528503ee292cb7742c18
Byron Gil <begil@cpp.edu>
2018-06-04 05:15:52 +0000

    adding crown for admin account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @hasperm = @user.privilege
     @comm = Comment.where('wall_id' => @user.userID )
     @icomments = @user.IComments.where.not('issue_id' => nil )
     store_location
